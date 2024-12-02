186343360c29d272bfc8706e479fa27ce4298e3d
doyeon0307 <eg4rang@naver.com>
2024-12-02 16:56:10 +0900

    fix: key.go

--- config/key.go
+++ config/key.go
@@ -15,14 +15,16 @@ var (
 )
 
 func init() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
+	_ = godotenv.Load()
 
+	// 환경변수 읽기
 	AWS_ACCESS_KEY = os.Getenv("AWS_ACCESS_KEY")
 	AWS_SECRET_KEY = os.Getenv("AWS_SECRET_KEY")
 	JWT_SECRET_KEY = os.Getenv("JWT_SECRET_KEY")
+
+	if AWS_ACCESS_KEY == "" || AWS_SECRET_KEY == "" || JWT_SECRET_KEY == "" {
+		log.Fatal("Required environment variables are not set")
+	}
 
 	fmt.Println("AWS_ACCESS_KEY:", AWS_ACCESS_KEY)
 	fmt.Println("AWS_SECRET_KEY:", AWS_SECRET_KEY)
